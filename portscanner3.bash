507615a08a5accd5d2b9d5bd3b8747419138c84e
Fatima <97912341+FatimaOmerovic@users.noreply.github.com>
2025-01-28 11:12:34 -0500

    Create portscanner3.bash

---
+++ portscanner3.bash
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+hostfile=$1
+portfile=$2
+echo "host,port"
+for host in {1..254}; do
+        timeout .1 bash -c "echo >/dev/tcp/$hostfile.$host/$portfile" 2>/dev/null &&
+            echo "$hostfile.$host,$portfile"
+done
